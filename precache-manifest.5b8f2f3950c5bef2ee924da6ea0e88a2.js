be1a5c6d516c20c9c099ac83aa958273aaa979bf
grandmullah <collinsbiegon9@gmail.com>
2019-04-24 16:54:07 +0300

    Updates

---
+++ precache-manifest.5b8f2f3950c5bef2ee924da6ea0e88a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb3354d1e787ae4e0ad6",
+    "url": "/frontend-land/static/js/runtime~main.b0a064aa.js"
+  },
+  {
+    "revision": "5cdc8534e5070d6da6af",
+    "url": "/frontend-land/static/js/main.70a1143d.chunk.js"
+  },
+  {
+    "revision": "d3abb9039d5cb10b5b6d",
+    "url": "/frontend-land/static/js/2.3f7fa86f.chunk.js"
+  },
+  {
+    "revision": "5cdc8534e5070d6da6af",
+    "url": "/frontend-land/static/css/main.c8780a1d.chunk.css"
+  },
+  {
+    "revision": "d3abb9039d5cb10b5b6d",
+    "url": "/frontend-land/static/css/2.2378dde4.chunk.css"
+  },
+  {
+    "revision": "2a329070e2af8e3a8b623d1278115cc0",
+    "url": "/frontend-land/index.html"
+  }
+];
